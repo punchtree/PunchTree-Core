4180e3f18833d1bceb42ebb35ec23dacab0a55d1
Cxom <cxomtdoh@yahoo.com>
2016-05-10 00:09:02 -0500

    Add Plugin getter and plugin.yml

--- src/net/punchtree/core/PunchtreeCore.java
+++ src/net/punchtree/core/PunchtreeCore.java
@@ -1,7 +1,13 @@
 package net.punchtree.core;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PunchtreeCore extends JavaPlugin {
+	
+	public static Plugin getPlugin(){
+		return Bukkit.getPluginManager().getPlugin("Punchtree-Core");
+	}
 	
 }
