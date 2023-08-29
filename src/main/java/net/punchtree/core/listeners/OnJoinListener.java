702fa315e65dc0beb940ef0e8888280eafb85aef
Jeremy Karevich <cxomtdoh@yahoo.com>
2023-08-29 17:31:42 -0400

    late-initialize spawn location field and add bypass spawn on join permission

--- src/main/java/net/punchtree/core/listeners/OnJoinListener.java
+++ src/main/java/net/punchtree/core/listeners/OnJoinListener.java
@@ -1,5 +1,7 @@
 package net.punchtree.core.listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,7 +12,12 @@ import net.md_5.bungee.api.ChatColor;
 
 public class OnJoinListener implements Listener {
 
-//	private static final Location MINIGAMES_SPAWN = new Location(Bukkit.getWorld("Quarantine"), )
+	private static final String BYPASS_SPAWN_ON_JOIN_PERMISSION = "bypass.spawn_on_join";
+	private static Location MINIGAMES_SPAWN;
+
+	public OnJoinListener() {
+		MINIGAMES_SPAWN = new Location(Bukkit.getWorld("Quarantine"), 4000.5, 71, -2499.5);
+	}
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
@@ -19,8 +26,16 @@ public class OnJoinListener implements Listener {
 //		PunchTreePlayer ptp = getPunchtreePlayer(player);
 
 		event.joinMessage(Component.text(ChatColor.RED + player.getName() + " is playing minigames?!"));
+
+		if (!canBypassSpawnOnJoin(player)) {
+			player.teleport(MINIGAMES_SPAWN);
+		}
 //		player.teleport(MINIGAMES_SPAWN);
 //		displayScoreboard(player);
+	}
+
+	private boolean canBypassSpawnOnJoin(Player player) {
+		return player.hasPermission(BYPASS_SPAWN_ON_JOIN_PERMISSION);
 	}
 
 }
